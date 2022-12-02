eed2bdadb328ca0cbc6620489c3b7c1de05eb501
shiqugao <1147965943@qq.com>
2022-12-02 14:25:24 +0000

    i have combine these two function and set situation for each conditions

--- Code/demos/LED_score/LED_score.ino
+++ Code/demos/LED_score/LED_score.ino
@@ -2,12 +2,18 @@
 
 #define LED_PIN     14
 #define NUM_LEDS    20
-
+#include <Servo.h>
+int tempPin = 12;
 CRGB leds[NUM_LEDS];
+
+int pos;
 
 
 int a = 5;
 int b = 3;
+
+char current = False;
+char used = False;
 void setup() {
 
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
@@ -16,14 +22,64 @@ void setup() {
 
 void loop() {
 
+ LEDgoals();
+ if (current != used & a < b){
+   servoflag1();
+   //update the used one into new one
+ }
+ if (current != used & a > b){
+   servoflag2();
+   //update the used one into new one
+ }
+ if(current != used & a = b){
+   myservo.write(0);
+   //update the used one into new one
+}
+
+void LEDgoals(){
   for (int i = 0; i <= a-1; i++) {
-    leds[i] = CRGB ( 0, 0, 255);
+    leds[i] = CRGB ( 0, 0, 254);
     FastLED.show();
-    delay(40);
+    delay(200);
   }
-  for (int i = 8; i <= 7+b; i++) {
-    leds[i] = CRGB ( 255, 0, 0);
+  for (int x = 15; x <= 14+b; x++) {
+    leds[x] = CRGB ( 255, 0, 0);
     FastLED.show();
-    delay(40);
+    delay(200);
   }
+}
+
+
+void servoFlag1(){
+        myservo.write(0);
+        delay(1000);
+        myservo.write(180);
+        delay(1000);
+        myservo.write(0);
+        delay(1000);
+        myservo.write(180);
+        delay(1000);
+        myservo.write(0);
+        delay(1000);
+        myservo.write(180);
+        delay(1000);
+        myservo.write(45);
+      }
+}
+
+void servoFlag2(){
+        myservo.write(0);
+        delay(1000);
+        myservo.write(180);
+        delay(1000);
+        myservo.write(0);
+        delay(1000);
+        myservo.write(180);
+        delay(1000);
+        myservo.write(0);
+        delay(1000);
+        myservo.write(180);
+        delay(1000);
+        myservo.write(135);
+      }
 }
