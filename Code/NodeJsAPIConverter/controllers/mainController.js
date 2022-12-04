5a1d7582feef61245d7802ffbc47fe0cdc60a7fc
Yaman Kalaji <syk.yaman@gmail.com>
2022-12-04 22:17:18 +0000

    Finish the fake MQTT scenario in the NodeJS converter

--- Code/NodeJsAPIConverter/controllers/mainController.js
+++ Code/NodeJsAPIConverter/controllers/mainController.js
@@ -13,7 +13,64 @@ export default class mainController {
  async realMQTT(req, res) {
     try{
       
+      const client = mqtt.connect(connectUrl, {
+        clientId,
+        clean: true,
+        connectTimeout: 4000,
+        username: MQTT_USER_NAME,
+        password: MQTT_PASSWORD,
+        reconnectPeriod: 1000,
+      })
+      client.publish('student/CASA0022/whatscup/real/currentMatch', '{"home_score":1, "away_score":0, "home_team_en":"Senegal", "away_team_en": "Nederland", "finished": "true", "datetime": "2022-11-21 13:00"}')
       
+      
+      res.send("Success");
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+  }
+
+  async fakeMQTT(req, res) {
+    
+    try{
+      const fakeDataArray = [
+        `{"home_score":0, 
+        "away_score":0, 
+        "home_team_en":"England", 
+        "away_team_en": "France", 
+        "finished": "false", 
+        "datetime": "2022-11-21 13:01"
+      }`,
+      `{"home_score":1, 
+        "away_score":0, 
+        "home_team_en":"England", 
+        "away_team_en": "France", 
+        "finished": "false", 
+        "datetime": "2022-11-21 13:01"
+      }`,
+      `{"home_score":2, 
+        "away_score":0, 
+        "home_team_en":"England", 
+        "away_team_en": "France", 
+        "finished": "false", 
+        "datetime": "2022-11-21 13:02"
+      }`,
+      `{"home_score":2, 
+        "away_score":1, 
+        "home_team_en":"England", 
+        "away_team_en": "France", 
+        "finished": "false", 
+        "datetime": "2022-11-21 13:03"
+      }`,
+      `{"home_score":3, 
+        "away_score":1, 
+        "home_team_en":"England", 
+        "away_team_en": "France", 
+        "finished": "true", 
+        "datetime": "2022-11-21 13:04"
+      }`
+    ]
 
       const client = mqtt.connect(connectUrl, {
         clientId,
@@ -30,7 +87,7 @@ export default class mainController {
         index = 0;
         cache.put('fake-index', '0');
       } 
-      client.publish('student/CASA0022/whatscup/fake/currentMatch', '')
+      client.publish('student/CASA0022/whatscup/fake/currentMatch', fakeDataArray[parseInt(index)])
       
       //moving to the next fake item
       if(index === 4)
