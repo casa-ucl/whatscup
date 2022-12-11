b01837647d4732176494c0484b87fd27ea91ae37
shiqugao <1147965943@qq.com>
2022-12-11 12:11:25 +0000

    finish LED function with blinking

--- Code/demos/LED_score/LED_score.ino
+++ Code/demos/LED_score/LED_score.ino
@@ -8,8 +8,8 @@
 
 #include <FastLED.h>
 #include <Servo.h>
-#define LED_PIN     14
-#define NUM_LEDS    24
+#define LED_PIN 14
+#define NUM_LEDS 24
 
 CRGB leds[NUM_LEDS];
 Servo myservo;
@@ -20,23 +20,24 @@ int pos;
 String current = "false";
 String used = "false";
 
-const char* ssid     = SECRET_SSID;
+const char* ssid = SECRET_SSID;
 const char* password = SECRET_PASS;
 const char* mqttuser = SECRET_MQTTUSER;
 const char* mqttpass = SECRET_MQTTPASS;
 
 ESP8266WebServer server(80);
 const char* mqtt_server = "mqtt.cetools.org";
-WiFiClient espClient;//handle wifi messages
-PubSubClient client(espClient);//handle MQTT messages, pass wificlient to connect
-StaticJsonDocument<200> doc;// Allocate the JSON document
+WiFiClient espClient;            //handle wifi messages
+PubSubClient client(espClient);  //handle MQTT messages, pass wificlient to connect
+StaticJsonDocument<200> doc;     // Allocate the JSON document
 
 void setup() {
+
   Serial.begin(115200);
   delay(100);
 
-  startWifi(); 
-  startWebserver(); 
+  startWifi();
+  startWebserver();
 
   // start MQTT server
   client.setServer(mqtt_server, 1884);
@@ -44,8 +45,7 @@ void setup() {
 
   myservo.attach(12);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-
-}  
+}
 
 
 
@@ -60,13 +60,13 @@ void startWifi() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-  } 
-  
+  }
+
   //Exit the while loop means have a connection
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP()); //IP address of Huzzah
+  Serial.println(WiFi.localIP());  //IP address of Huzzah
 }
 
 void startWebserver() {
@@ -84,29 +84,23 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println();
 
-  StaticJsonDocument<1024> doc;// Allocate the JSON document
+  StaticJsonDocument<1024> doc;  // Allocate the JSON document
   String myString = String((char*)payload);
   //char json[length] = myString;
 
-  deserializeJson(doc, myString); // Deserialize the JSON document
+  deserializeJson(doc, myString);  // Deserialize the JSON document
 
   // Fetch values.
-  int home_score = doc["home_score"]; 
+  int home_score = doc["home_score"];
   int away_score = doc["away_score"];
   String home_team_en = doc["home_team_en"];
-  String away_team_en = doc["away_team_en"];  
+  String away_team_en = doc["away_team_en"];
   String finished = doc["finished"];
   String datatime = doc["datetime"];
 
   a = home_score;
   b = away_score;
   current = finished;
-
-
-
-
-   
-
 }
 
 void reconnect() {
@@ -116,11 +110,11 @@ void reconnect() {
     // Create a random client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
-    
+
     // Attempt to connect with clientID, username and password
     if (client.connect(clientId.c_str(), mqttuser, mqttpass)) {
       Serial.println("connected");
-      client.subscribe("student/ucfnnbx/whatscup/match");//subscribe to the topic
+      client.subscribe("student/ucfnnbx/whatscup/match");  //subscribe to the topic
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -135,106 +129,128 @@ void reconnect() {
 
 
 
-void LEDgoals1(){
-  if (a>0){
-  for (int i = 0; i <= a-1; i++) {
-    leds[i] = CRGB ( 0, 0, 254);
-    FastLED.setBrightness(10);
-    FastLED.show();
-    delay(200);
+void LEDgoals1() {
+  Serial.println("yy6");
+  FastLED.clearData();
+  if (a > 0) {
+    //FastLED.clearData();
+    Serial.println("yy7");
+    // fill_solid(leds, a, CRGB::Black);
+    for (int i = 0; i <= a -1; i++) {
+      Serial.println("yy8");
+      leds[i] = CRGB(100, 100, 254);
+      Serial.println(i);
+      FastLED.setBrightness(10);
+      FastLED.show();
+      delay(200);
+    }
   }
-  }
-  else{
-      for (int i = 0; i <= 14; i++) {
-    leds[i] = CRGB ( 0, 0, 0);
-    FastLED.show();
-    delay(200);
- }
-  }
+  // if (a == 0) {
+  //   Serial.println("yy9");
+  //   //FastLED.clearData();
+  //   for (int i = 0; i <= 14; i++) {
+  //     Serial.println("yy10");
+  //     leds[i] = CRGB(0, 0, 0);
+  //     FastLED.show();
+  //     delay(200);
+   // }
+  //}
 }
-void LEDgoals2(){
-
- if (b>0){
-  for (int x = 15; x <= 14+b; x++) {
-    leds[x] = CRGB ( 255, 0, 0);
-    FastLED.setBrightness(10);
-    FastLED.show();
-  delay(200);
+void LEDgoals2() {
+  FastLED.clearData();
+  if (b > 0) {
+    //FastLED.clearData();
+    for (int x = 15; x <= 14 + b; x++) {
+      leds[x] = CRGB(255, 100, 100);
+      FastLED.setBrightness(10);
+      FastLED.show();
+      delay(200);
+    }
   }
- }
- else{
-   for (int x = 15; x <= 24; x++) {
-     //change 24 when create the final version
-    leds[x] = CRGB ( 0, 0, 0);
 
-    FastLED.show();
-  delay(200); 
- }
+  // if (b == 0) {
+  //   //FastLED.clearData();
+  //   for (int x = 15; x <= 24; x++) {
+  //     //change 24 when create the final version
+  //     leds[x] = CRGB(0, 0, 0);
+  //     delay(200);
+  //     FastLED.show();
+  //   }
+  // }
 }
-  }
 
-void servoFlag1(){
-        myservo.write(0);
-        delay(1000);
-        myservo.write(180);
-        delay(1000);
-        myservo.write(0);
-        delay(1000);
-        myservo.write(180);
-        delay(1000);
-        myservo.write(0);
-        delay(1000);
-        myservo.write(180);
-        delay(1000);
-        myservo.write(45);
-      }
+void servoFlag1() {
+  myservo.write(0);
+  delay(1000);
+  myservo.write(180);
+  delay(1000);
+  myservo.write(0);
+  delay(1000);
+  myservo.write(180);
+  delay(1000);
+  myservo.write(0);
+  delay(1000);
+  myservo.write(180);
+  delay(1000);
+  myservo.write(45);
+}
 
 
-void servoFlag2(){
-        myservo.write(0);
-        delay(1000);
-        myservo.write(180);
-        delay(1000);
-        myservo.write(0);
-        delay(1000);
-        myservo.write(180);
-        delay(1000);
-        myservo.write(0);
-        delay(1000);
-        myservo.write(180);
-        delay(1000);
-        myservo.write(135);
-      }
+void servoFlag2() {
+  myservo.write(0);
+  delay(1000);
+  myservo.write(180);
+  delay(1000);
+  myservo.write(0);
+  delay(1000);
+  myservo.write(180);
+  delay(1000);
+  myservo.write(0);
+  delay(1000);
+  myservo.write(180);
+  delay(1000);
+  myservo.write(135);
+}
 
 
 void loop() {
-  
+
   // handler for receiving requests to webserver
-  server.handleClient(); //?
-  if (!client.connected()) {//check if connected to MQTT
+  server.handleClient();      //?
+  if (!client.connected()) {  //check if connected to MQTT
     reconnect();
   }
 
   client.loop();
-  LEDgoals1();
-  LEDgoals2();
- if ((current != used) && (a < b)){
-   servoFlag1();
-   used = current;
-   delay(1000);
-   //update the used one into new one
- }
-  if ((current != used) && (a > b)){
+  Serial.println("yy1");
+  Serial.println(a);
+
+  if ((current != used) && (a < b)) {
+    servoFlag1();
+    used = current;
+    delay(1000);
+    Serial.println("yy2");
+    //update the used one into new one
+  }
+  if ((current != used) && (a > b)) {
     servoFlag2();
     used = current;
     delay(1000);
+    Serial.println("yy3");
+    Serial.println(a);
     //update the used one into new one
   }
-  if( (current != used) && (a = b)){
+  if ((current != used) && (a == b)) {
     myservo.write(0);
     used = current;
     delay(1000);
+    Serial.println("yy4");
     //update the used one into new one
-}
- 
+  }
+  Serial.println("yy5");
+  delay(1000);
+  LEDgoals1();
+
+  LEDgoals2();
+  Serial.println("yy11");
 }
